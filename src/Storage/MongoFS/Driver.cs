ec241f9a9f13b2246d7d9f2fb0cab82d6f5e566f
François BOITEUX <francois.boiteux@epita.fr>
2015-05-24 19:16:30 +0200

    [MONGOFS] Add 2 bootstrap project for MongoFS storage. StorageDebuggerConsole use the dll generated by Storage project and start MongoFS server to perform download/upload/cleanall

---
+++ src/Storage/MongoFS/Driver.cs
@@ -0,0 +1,112 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.GridFS;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.MongoFS
+{
+    public class Driver
+    {
+        private MongoServer server;
+        private MongoDatabase database;
+        private MongoGridFS gridFS;
+
+        public Driver()
+        {
+            string url = ConfigurationManager.AppSettings["mongoUrl"];
+            string db = ConfigurationManager.AppSettings["mongoDb"];
+            this.server = MongoServer.Create(url);
+            this.database = server.GetDatabase(db);
+            this.gridFS = database.GridFS;
+        }
+
+        public void UploadFile(string fileName)
+        {
+            try
+            {
+                using (var fs = new FileStream(fileName, FileMode.Open))
+                {
+
+                    var gridFsInfo = gridFS.Upload(fs, fileName);
+                    var fileId = gridFsInfo.Id;
+                }
+
+            }
+            catch (MongoConnectionException e)
+            {
+                DriverException exception = new DriverException(e.Message, e);
+                exception.ExplainProblem();
+            }
+        }
+
+        public void CleanAll()
+        {
+            this.database.Drop();
+        }
+
+        public void GetFileAndSaveTo(string inputFile, string outputFile)
+        {
+            try
+            {
+                var file = gridFS.FindOne(Query.EQ("filename", inputFile));
+                using (var stream = file.OpenRead())
+                {
+                    var bytes = new byte[stream.Length];
+                    stream.Read(bytes, 0, (int)stream.Length);
+                    using (var newFs = new FileStream(outputFile, FileMode.Create))
+                    {
+                        newFs.Write(bytes, 0, bytes.Length);
+                    }
+                }
+            }
+            catch (MongoConnectionException e)
+            {
+                DriverException exception = new DriverException(e.Message, e);
+                exception.ExplainProblem();
+            }
+        }
+
+        public byte[] GetFileBytes(string inputFile, string outputFile)
+        {
+            try
+            {
+                var file = gridFS.FindOne(Query.EQ("filename", inputFile));
+                using (var stream = file.OpenRead())
+                {
+                    byte[] bytes = new byte[stream.Length];
+                    stream.Read(bytes, 0, (int)stream.Length);
+                    return bytes;
+                }
+            }
+            catch (MongoConnectionException e)
+            {
+                DriverException exception = new DriverException(e.Message, e);
+                exception.ExplainProblem();
+                return null;
+            }
+        }
+
+        public void ListFiles()
+        {
+            try
+            {
+                foreach (MongoGridFSFileInfo gridFsInfo in gridFS.FindAll())
+                {
+                    Console.WriteLine(gridFsInfo.Name);
+                }
+            }
+            catch (MongoConnectionException e)
+            {
+                DriverException exception = new DriverException(e.Message, e);
+                exception.ExplainProblem();
+            }
+        }
+    }
+}
---
+++ src/Storage/MongoFS/DriverException.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.MongoFS
+{
+    public class DriverException : MongoConnectionException
+    {
+        public DriverException(string message, MongoConnectionException innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public void ExplainProblem()
+        {
+            Console.WriteLine("Exception -> Start the mongoFS Server (mongod.exe) in MongoServer folder");
+        }
+    }
+}
