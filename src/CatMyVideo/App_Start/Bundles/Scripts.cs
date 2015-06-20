e281090f848cb0402d1f4c8b9d94bc0a2bb10bed
unknown <manuel.waxel@gmail.com>
2015-06-20 19:39:53 +0200

    [FRONT] Archetype done for SASS and Coffeescript

---
+++ src/CatMyVideo/App_Start/Bundles/Scripts.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Optimization;
+
+namespace CatMyVideo.Bundles
+{
+    public static class Coffescript
+    {
+        public static Bundle Scripts()
+        {
+            return new ScriptBundle("~/bundles/coffee").Include(
+                      "~/Scripts/coffee/Site.js");
+        }
+    }
+}
