4e8965c90a0233b97bc40c4fcd888fe535dfe76d
Seika <nocteaestiva@gmail.com>
2015-07-01 03:29:56 +0200

    [FRONT] Video view: management buttons and view counter added

--- src/CatMyVideo/Controllers/VideoController.cs
+++ src/CatMyVideo/Controllers/VideoController.cs
@@ -11,9 +11,27 @@ namespace CatMyVideo.Controllers
     // GET: Video
     public ActionResult Index(int id = 1)
     {
-      Engine.Dbo.Video video = Engine.BusinessManagement.Video.GetVideo(id);
+      // Mock
+      ViewBag.Username = "Seika";
+      ViewBag.CanDelete = true;
+      ViewBag.CanEdit = true;
+      var video = new Engine.Dbo.Video()
+      {
+        Id = 1,
+        Title = "Wolf Mountain",
+        ViewCount = 1329211,
+        Description = "Integer vehicula sagittis ipsum, in tempor arcu sollicitudin eget. Suspendisse efficitur, elit a eleifend tempor, enim velit accumsan elit, eu vehicula risus orci sit amet sem. Maecenas non risus eget lectus feugiat gravida tincidunt vel libero. Proin ut sem lacus. Proin odio felis, venenatis ac sodales id, rutrum sit amet lectus. Suspendisse ut sem et elit ornare dignissim ac in nisi. In at nisi arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin faucibus mi varius lorem lobortis, lobortis pretium ipsum accumsan. Morbi sit amet risus risus.",
+      };
+
+      // Set privileges
+      //Engine.Dbo.Video video = Engine.BusinessManagement.Video.GetVideo(id);
+      //var user = Engine.BusinessManagement.User.FindUser(video.Id);
+      //ViewBag.Username = user.Nickname;
+      //ViewBag.CanDelete = user.Type > Engine.Dbo.User.Role.Classic || User.Identity.Name == user.Nickname;
+      //ViewBag.CanEdit = user.Type > Engine.Dbo.User.Role.Modo || User.Identity.Name == user.Nickname;
+
       ViewBag.Updated = false;
-      return View();
+      return View(video);
     }
 
     [Route("Video/Edit/{id}")]
