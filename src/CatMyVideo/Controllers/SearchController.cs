10fb7d0db5be8b14f261920965fb62b31ff64581
Seika <nocteaestiva@gmail.com>
2015-07-02 14:34:21 +0200

    [FRONT] Search feature implemented

---
+++ src/CatMyVideo/Controllers/SearchController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CatMyVideo.Controllers
+{
+  public class SearchController : Controller
+  {
+    // GET: Search
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult Browse()
+    {
+      var query = HttpContext.Request.Params.Get("query");
+      ViewBag.query = query;
+      /* Fancy stuff -- implement that later.
+       * var page = 0;
+       * const int NB_VIDEO_BY_PAGE = 20;
+       * var pageParam = HttpContext.Request.Params.Get("p");
+       * if (pageParam != null)
+       *  Int32.TryParse(pageParam, out page); */
+
+      // Fetch videos by tags
+      var tags = query.Split(' ').Select(x => new Engine.Dbo.Tag() { Name = x }).ToList();
+      var videosTags = Engine.BusinessManagement.Video.ListVideosByTags(tags).ToList();
+      ViewData["videos_tags"] = videosTags;
+
+      // Fetch videos by user
+      var videosUser = Engine.BusinessManagement.Video.ListVideosByAuthor(query);
+      ViewData["videos_user"] = videosUser;
+
+      // Fetch videos by name
+      var videosName = Engine.BusinessManagement.Video.ListVideosByName(query).ToList();
+      ViewData["videos_name"] = videosName;
+
+      return View();
+    }
+  }
+}
